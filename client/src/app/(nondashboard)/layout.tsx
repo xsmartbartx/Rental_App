e90aa9fadb88cd53bb6d66cb3142c390cd144bbb
xsmartbartx <73461850+xsmartbartx@users.noreply.github.com>
2026-02-13 22:33:03 +0100

    1 stage

--- client/src/app/(nondashboard)/layout.tsx
+++ client/src/app/(nondashboard)/layout.tsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { Children } from "react";
 
 const Layout = () => {
     return (
-        <div>Layout</div>
-    )
-}
+        <div>
+            {/*<Navbar /> */}
+            navbar
+            <main className={'h-full w-flex w-full flex-col pt-[${NAVBAR_HEIGHT}px]'}>
+                {children}
+            </main>
+        </div>
+    );
+};
 
 export default Layout;
